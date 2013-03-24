4292ea3789fb23ced041c7eba71f3fc146b0d683
John Figueiredo <johnfigueiredo@Johns-MacBook-Pro.local>
2013-03-23 19:37:42 -0700

    first commit

---
+++ gitconfig.rb
@@ -0,0 +1,17 @@
+[user]
+	name = John Figueiredo
+	email = john@johnfigueiredo.com
+[alias]
+	co = checkout
+	ci = commit
+	st = status
+	br = branch
+    hist = log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short
+	go = checkout
+	type = cat-file
+	dump = cat-file
+[core]
+	editor = subl -w
+
+[credential]
+	helper = osxkeychain
